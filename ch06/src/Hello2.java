08d2b7decdc2c5c1c2c6de1eac4191dc41d846dd
TaeDongUm <etd93@naver.com>
2023-05-05 16:53:25 +0900

    5_5 study

---
+++ ch06/src/Hello2.java
@@ -0,0 +1,9 @@
+
+//public class Hello2 {}
+class Hello3{}
+//public class Hello4{} Hello4 must be in its own file 하나의 소스파일에 하나의 public 클래스만 허용
+class hello2 {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
