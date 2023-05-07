bcca975fc5f9c09b31a8b9eaaa7bfdb6a50c6f04
TaeDongUm <etd93@naver.com>
2023-05-07 23:38:34 +0900

    5_7 study

--- ch06/src/Ex6_4.java
+++ ch06/src/Ex6_4.java
@@ -19,6 +19,16 @@ class Ex6_4 {
 		return result;
 	//	return a + b;	// 위의 두 줄을 이와 같이 한 줄로 간단히 할 수 있다.
 	}
+	// 두 값을 받아서 둘 중에 큰 값을 반환하는 메서드를 작성하시오.
+	long max(long a, long b) {
+		long result = 0;
+		if(a>b) {
+			result = a;
+		} else {
+			result = b;
+		}
+		return result;
+	}
 	long subtract(long a, long b) { return a - b; }
 	long multiply(long a, long b) { return a * b; }
 	double divide(double a, double b) {
