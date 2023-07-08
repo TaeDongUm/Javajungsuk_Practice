73b960a0fba56c4f6138d7e1eaa289e06fe2b2c6
TaeDongUm <etd93@naver.com>
2023-07-08 22:39:53 +0900

    7_8 Javastudy

---
+++ ch06/src/ch6_practice.java
@@ -0,0 +1,16 @@
+public class ch6_practice {
+	public static void main(String args[]) {
+		// 객체지향적인 코드
+		Time t = new Time();
+		t.hour = 12;
+		t.minute = 34;
+		t.second = 56;
+	}
+}
+
+// 
+class Time{
+	int hour;
+	int minute;
+	int second;
+}
